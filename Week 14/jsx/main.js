ab267e190c76ee817edb8cc501533e202316d9aa
gushengnan <shengnan@buffalo.edu>
2021-01-26 01:02:42 +0800

    feat: complete code with bug

--- Week 14/jsx/main.js
+++ Week 14/jsx/main.js
@@ -20,15 +20,36 @@ class Carousel extends Component {
             this.root.appendChild(child);
         }
 
+        let position = 0;
+
         this.root.addEventListener('mousedown', e => {
-            console.log('mousedown');
+            let children = this.root.children;
+
+            let startX = e.clientX;
+            // let startY = e.clientY;
 
             let move = e => {
-                console.log('mousemove');
+                let x = e.clientX - startX;
+
+                let current = position - ((x - x % 500) / 500);
+
+                for (let offset of [-1, 0, 1]) {
+                    let pos = current + offset;
+                    pos = (pos + children.length) % children.length;
+                    children[pos].style.transition = 'none';
+                    children[pos].style.transform = `translateX(${- pos * 500 + offset * 500 + x % 500}px)`;
+                }
             }
 
             let up = e => {
-                console.log('mouseup')
+                let x = e.clientX - startX;
+                position = position - Math.round(x / 500);
+                for (let offset of [0, - Math.sign(Math.round(x / 500) - x + 250 * Math.sign(x))]) {
+                    let pos = position + offset;
+                    pos = (pos + children.length) % children.length;
+                    children[pos].style.transition = '';
+                    children[pos].style.transform = `translateX(${- pos * 500 + offset * 500}px)`;
+                }
                 document.removeEventListener('mousemove', move);
                 document.removeEventListener('mousemove', up);
             }
