fa5d561c8b3eb823d2a13f4b3c44735f006d6db3
gushengnan <gushengnan@buffalo.edu>
2021-03-14 21:09:32 +0800

    feat: complete oauth code

--- Week 19/publish-server/server.js
+++ Week 19/publish-server/server.js
@@ -1,9 +1,85 @@
-let http = require('http');
+const http = require('http');
+const https = require('https');
 const unzipper = require('unzipper');
+const querystring = require('querystring');
+
+const clientId = 'Iv1.a551f54c9e63b8ce';
+const clientSecret = '1175545d7e7ba6f656d51013e72cdf5bac9ff5db';
+
+// 2 auth路由：接受 code，用 code、client_id、client_sectret 换 token
+function auth(request, response) {
+    let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    getToken(query.code, info => {
+        response.write(`<a href="http://localhost:8083/?token=${info.access_token}">publish</a>`)
+        response.end();
+    });
+}
+
+function getToken(code, callback) {
+    let request = https.request({
+        hostname: 'github.com',
+        path: `/login/oauth/access_token?code=${code}&client_id=${clientId}&client_secret=${clientSecret}`,
+        port: 443,
+        method: 'POST',
+    }, (response) => {
+        let body = '';
+        response.on('data', chunk => {
+            body += chunk.toString();
+        });
+        response.on('end', chunk => {
+            if (chunk) {
+                body += chunk.toString();
+            }
+            let o = querystring.parse(body);
+            callback(o);
+        });
+    });
+    request.end();
+}
+
+// 4 publish路由：用 token 获取用户信息，检查权限 接受发布
+function getUser(token, callback) {
+    let request = https.request({
+        hostname: 'api.github.com',
+        path: '/user',
+        port: 443,
+        method: 'POST',
+        headers: { 'Authorization': `token ${token}` }
+    }, (response) => {
+        let body = '';
+        response.on('data', chunk => {
+            body += chunk.toString();
+        });
+        response.on('end', chunk => {
+            if (chunk) {
+                body += chunk.toString();
+            }
+            callback(JSON.parse(body));
+        });
+    });
+    request.end();
+}
+
+function publish(request, response) {
+    let query = querystring.parse(request.url.match(/^\/auth\?([\s\S]+)$/)[1]);
+    if (query.token) {
+        getUser(query.token, info => {
+            if (info.login && info.login === 'gushengnan') {
+                request.pipe(unzipper.Extract({ path: '../server/public/' }));
+                request.on('end', () => {
+                    response.end('success');
+                });
+            }
+        });
+    }
+}
 
 http.createServer(function (request, response) {
-    console.log('request received')
-    request.pipe(unzipper.Extract({ path: '../server/public/' }));
-    response.write('success');
+    if (request.url.match(/^\/auth\?/)) {
+        return auth(request, response);
+    }
 
+    if (request.url.match(/^\/publish\?/)) {
+        return publish(request, response);
+    }
 }).listen(8082);
--- Week 19/publish-tool/publish.js
+++ Week 19/publish-tool/publish.js
@@ -1,26 +1,42 @@
-let http = require('http');
-let fs = require('fs');
+const http = require('http');
+const fs = require('fs');
 const archiver = require('archiver');
+const child_process = require('child_process');
+const querystring = require('querystring');
 
-let request = http.request({
-    hostname: '127.0.0.1',
-    port: '8082',
-    method: 'POST',
-    headers: {
-        'Content-Type': 'application/octet-stream',
-        // 'Content-Length': stats.size
-    }
-}, response => {
-    console.log(response);
-});
+const githubClientId = 'Iv1.a551f54c9e63b8ce';
 
-const archive = archiver('zip', { zlib: { level: 9 } });
-archive.directory('./sample/', false);
-archive.finalize();
+// 1 打开 https://github.com/login/oauth/authorize
+// Mac 上用 open 命令
+child_process.exec(`start chrome.exe https://github.com/login/oauth/authorize?client_id=${githubClientId}`);
 
-archive.pipe(request);
+// 3 创建 server，接受 token，后点击发布
+http.createServer(function (request, response) {
+    let query = querystring.parse(request.url.match(/^\/\?([\s\S]+)$/)[1]);
+    publish(query.token);
+}).listen(8083);
 
-archive.on('end', () => {
-    request.end();
-});
+function publish(token) {
+    let request = http.request({
+        hostname: '127.0.0.1',
+        port: '8082',
+        method: 'GET',
+        path: `/publish?token=${token}`,
+        headers: {
+            'Content-Type': 'application/octet-stream',
+            'User-Agent': 'toy-publish'
+        }
+    }, response => {
+        console.log(response);
+    });
 
+    const archive = archiver('zip', { zlib: { level: 9 } });
+    archive.directory('./sample/', false);
+    archive.finalize();
+
+    archive.pipe(request);
+
+    archive.on('end', () => {
+        request.end();
+    });
+}
