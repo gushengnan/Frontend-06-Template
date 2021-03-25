60f04da33b03470936e098380a02a389128d8883
gushengnan <shengnan@buffalo.edu>
2021-03-25 13:57:37 +0800

    feat: complete puppeteer code part

--- Week 20/headless-demo/main.js
+++ Week 20/headless-demo/main.js
@@ -1,18 +1,12 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
 
-const USER_DATA_DIR = './.puppeteer_user_data';
-
 (async () => {
-    const browser = await puppeteer.launch({
-        // executablePath: path.resolve('/mnt/c/Program Files (x86)/Google/Chrome/Application/chrome.exe'),
-        userDataDir: USER_DATA_DIR,
-        // args: ['--no-sandbox', '--disable-setuid-sandbox']
-    });
+    const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto('https://www.baidu.com');
-    let a = await page.$('document');
-    console.log(a);
+    await page.goto('https://localhost:8080/main.html');
+    let imgs = await page.$$('a');
+    console.log(imgs);
 
     await browser.close();
 })();
